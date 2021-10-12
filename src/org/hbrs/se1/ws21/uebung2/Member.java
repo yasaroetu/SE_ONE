e84debbd36b16c940b7de408749b1be9442c371c
Yasar Özertürk <yasar.oezertuerk@smail.inf.h-brs.de>
2021-10-12 20:03:15 +0200

    - Uebung02 wurde gepullt.

--- src/org/hbrs/se1/ws21/uebung2/Member.java
+++ src/org/hbrs/se1/ws21/uebung2/Member.java
@@ -5,6 +5,7 @@ public interface Member {
     // ID ist über einen Konstruktor einer abgeleiteten Klasse
     // explizit außerhalb der Container-Klasse zu belegen
     // --> Primärschlüssel zur Unterscheidung aller Member-Objekte
+
     Integer getID();
 
     public String toString();
